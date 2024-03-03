d985c4ad010ed6719e1dc54bd78c458e89068196
jonmejia <jonathan@dataminded.net>
2024-03-02 21:23:05 -0500

    feat: added pic of myself to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Image
+        className='rounded-full'
+        src={'/jonathan.jpg'}
+        width={250}
+        height={250}
+        alt='image of Jonathan' />
     </div>
   );
 }
