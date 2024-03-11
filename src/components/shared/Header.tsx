2bcc42fbd14f3e1ad56eaf96321f6fa4dd1e28c9
jonmejia <jonathan@dataminded.net>
2024-03-10 21:31:48 -0400

    style: moved links in header into seperate div and added padding

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,26 +1,34 @@
-'use client'
-import React from 'react';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+"use client";
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 const Header = (): JSX.Element => {
-
   const router = useRouter();
   return (
-    <div className='flex justify-between items-center px-4'>
+    <div className="flex justify-between items-center px-4">
       <Image
-        onClick={() => router.push('/')}
-        src='/greentea.svg'
-        alt='green tea cup'
+        onClick={() => router.push("/")}
+        src="/greentea.svg"
+        alt="green tea cup"
         width={60}
         height={60}
       />
-      <span onClick={() => router.push('/')}>Home</span>
-      <span onClick={() => router.push('/About')}>About</span>
-      <span onClick={() => router.push('/Projects')}>Projects</span>
-      <span onClick={() => router.push('/Contact')}>Contact Me</span>
+      <div>
+        <span className="px-4" onClick={() => router.push("/")}>
+          Home
+        </span>
+        <span className="px-4" onClick={() => router.push("/About")}>
+          About
+        </span>
+        <span className="px-4" onClick={() => router.push("/Projects")}>
+          Projects
+        </span>
+        <span className="px-4" onClick={() => router.push("/Contact")}>
+          Contact Me
+        </span>
+      </div>
     </div>
   );
-
 };
 
 export default Header;
