0787d84ab6febf273133ec0c499949ef4f9058b7
jonmejia <jonathan@dataminded.net>
2024-03-22 14:01:50 -0400

    feat: added header for project card cluster

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProjectCarousel from "../components/ProjectCarousel";
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-wrap justify-center my-16">
+      <div className="flex flex-wrap justify-center my-14">
         <Image
           className="rounded-full border-4 border-[#ADBC9F]"
           src={"/jonathan.jpg"}
@@ -16,6 +16,11 @@ export default function Home() {
           (keyboard for hire), and an avid vim enthusiast. Feel free to reach
           out and let's have a cup of tea some time :)
         </p>
+				<div></div>
+      </div>
+      <h2 className="text-center text-xl"> Recent Projects</h2>
+      <div className="flex justify-center">
+        <hr className="w-2/3 my-4" />
       </div>
       <ProjectCarousel />
     </main>
