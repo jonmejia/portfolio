f5b2cd38b424c03043b93e80eae7a8f6ef045ec4
jonmejia <jonathan@dataminded.net>
2024-03-22 19:06:08 -0400

    chore: remove contact me route in header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -3,33 +3,30 @@ import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 const Header = (): JSX.Element => {
-  const router = useRouter();
-  return (
-    <div className="flex justify-between items-center pl-4 pb-2">
-      <Image
-        className="flex ml-8"
-        onClick={() => router.push("/")}
-        src="/greentea.svg"
-        alt="green tea cup"
-        width={60}
-        height={60}
-      />
-      <div className="pr-8">
-        <span className="px-4" onClick={() => router.push("/")}>
-          Home
-        </span>
-        <span className="px-4" onClick={() => router.push("/About")}>
-          About
-        </span>
-        <span className="px-4" onClick={() => router.push("/Projects")}>
-          Projects
-        </span>
-        <span className="px-4" onClick={() => router.push("/Contact")}>
-          Contact Me
-        </span>
-      </div>
-    </div>
-  );
+	const router = useRouter();
+	return (
+		<div className="flex justify-between items-center pl-4 pb-2">
+			<Image
+				className="flex ml-8"
+				onClick={() => router.push("/")}
+				src="/greentea.svg"
+				alt="green tea cup"
+				width={60}
+				height={60}
+			/>
+			<div className="pr-8">
+				<span className="px-4" onClick={() => router.push("/")}>
+					Home
+				</span>
+				<span className="px-4" onClick={() => router.push("/About")}>
+					About
+				</span>
+				<span className="px-4" onClick={() => router.push("/Projects")}>
+					Projects
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
