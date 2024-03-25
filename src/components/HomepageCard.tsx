dd41445cebb253bdd759c52da3448b714d15662d
jonmejia <jonathan@dataminded.net>
2024-03-25 14:38:05 -0400

    feat: replaced localurl w githuburl + added gh icon that opens link to project

--- src/components/HomepageCard.tsx
+++ src/components/HomepageCard.tsx
@@ -1,29 +1,41 @@
 import React from "react";
 import Image from "next/image";
+import github from "../../public/github.svg";
 
 export const HomepageCard = (props: {
-        projectImage: string;
-        projectName: string;
-        prodUrl: string;
-        localUrl: string;
-        margin: string;
+  projectImage: string;
+  projectName: string;
+  prodUrl?: string;
+  githubUrl: string;
+  margin: string;
+  imageClasses?: string;
 }) => {
-        return (
-                <div
-                        className={`relative bg-[#12372A] rounded-lg aspect-square w-1/4 ${props.margin}`}
-                >
-                        <div className="text-center">{props.projectName}</div>
-                        <Image
-                                className="object-scale-down"
-                                src={props.projectImage}
-                                alt={"image"}
-                                width={500}
-                                height={500}
-                        />
-                        <div className="absolute opacity-0 hover:opacity-100">
-                                <div>{props.prodUrl}</div>
-                                <div>{props.localUrl}</div>
-                        </div>
-                </div>
-        );
+  const openNewTab = (url: string) => {
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div
+      className={`relative bg-[#12372A] rounded-lg aspect-square w-1/4 ${props.margin}`}
+    >
+      <div className="text-center pt-4">{props.projectName}</div>
+      <Image
+        className={`${props.imageClasses}`}
+        src={props.projectImage}
+        alt={"image"}
+        width={500}
+        height={500}
+      />
+      <div className="flex justify-center">
+        <Image
+          className="cursor-pointer"
+          onClick={() => openNewTab(props.githubUrl)}
+          src={github}
+          alt={"gitub icon"}
+          width={50}
+          height={50}
+        />
+      </div>
+    </div>
+  );
 };
