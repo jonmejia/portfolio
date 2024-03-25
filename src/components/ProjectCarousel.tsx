dd5ba9fa7767ea0affe9fa83ed2d8fb30312611a
jonmejia <jonathan@dataminded.net>
2024-03-25 14:38:52 -0400

    feat: finished first project card, replaced all local urls with github urls

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -2,29 +2,30 @@ import React from "react";
 import { HomepageCard } from "./HomepageCard";
 
 export default function ProjectCarousel() {
-	return (
-		<div className='flex justify-between'>
-			<HomepageCard
-				projectImage="img_string"
-				projectName="Project"
-				prodUrl="www.projectinvesting.com"
-				localUrl="localhost:4000"
-				margin='mx-20'
-			/>
-			<HomepageCard
-				projectImage="img_string"
-				projectName="Project"
-				prodUrl="www.projectinvesting.com"
-				localUrl="localhost:4000"
-				margin=''
-			/>
-			<HomepageCard
-				projectImage="img_string"
-				projectName="Project"
-				prodUrl="www.projectinvesting.com"
-				localUrl="localhost:4000"
-				margin='mx-20'
-			/>
-		</div>
-	);
+        return (
+                <div className="flex justify-between">
+                        <HomepageCard
+                                projectImage={"/teamux.png"}
+                                projectName="Teamux"
+                                prodUrl="https://www.github.com/jonmejia/teamux"
+                                githubUrl="https://www.github.com/jonmejia/teamux"
+                                margin="mx-20"
+				imageClasses="p-6"
+                        />
+                        <HomepageCard
+                                projectImage={"/teamux.png"}
+                                projectName="Project"
+                                prodUrl="www.projectinvesting.com"
+                                githubUrl="https://www.github.com/jonmejia/teamux"
+                                margin=""
+                        />
+                        <HomepageCard
+                                projectImage={"/teamux.png"}
+                                projectName="Project"
+                                prodUrl="www.projectinvesting.com"
+                                githubUrl="https://www.github.com/jonmejia/teamux"
+                                margin="mx-20"
+                        />
+                </div>
+        );
 }
