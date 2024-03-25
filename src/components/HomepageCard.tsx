3197f3014b6cbca66f315970027d97f4583500f9
jonmejia <jonathan@dataminded.net>
2024-03-25 14:14:45 -0400

    feat: changed bg color, centered title and moved to top of div + now using img string to pass in img

--- src/components/HomepageCard.tsx
+++ src/components/HomepageCard.tsx
@@ -1,20 +1,29 @@
-
 import React from "react";
+import Image from "next/image";
 
 export const HomepageCard = (props: {
-  projectImage: string;
-  projectName: string;
-  prodUrl: string;
-  localUrl: string;
-  margin: string;
+        projectImage: string;
+        projectName: string;
+        prodUrl: string;
+        localUrl: string;
+        margin: string;
 }) => {
-  return (
-    <div className={`bg-slate-500 rounded-lg aspect-square w-1/4 ${props.margin}`}>
-      <div className="opacity-0 hover:opacity-100">
-        <div>{props.prodUrl}</div>
-        <div>{props.localUrl}</div>
-      </div>
-      <div>{props.projectName}</div>
-    </div>
-  );
+        return (
+                <div
+                        className={`relative bg-[#12372A] rounded-lg aspect-square w-1/4 ${props.margin}`}
+                >
+                        <div className="text-center">{props.projectName}</div>
+                        <Image
+                                className="object-scale-down"
+                                src={props.projectImage}
+                                alt={"image"}
+                                width={500}
+                                height={500}
+                        />
+                        <div className="absolute opacity-0 hover:opacity-100">
+                                <div>{props.prodUrl}</div>
+                                <div>{props.localUrl}</div>
+                        </div>
+                </div>
+        );
 };
